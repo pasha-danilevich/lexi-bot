a1f920571812680bb2808d43033ade5683789681
pasha-danilevich <pasha-danilevich@outlook.com>
2024-12-12 23:54:32 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
 # src/utils.py
 
-def format_message(text):
-    return f"**{text}**"  # Пример форматирования текста (Markdown)
+
+def format_message(text: str, format_type: str) -> str:
+    switcher = {
+        'bold': f"**{text}**",  # Жирный текст
+        'spoiler': f"||{text}||",  # Спойлер
+        'italic': f"*{text}*",  # Курсив
+        'underline': f"__{text}__",  # Подчеркнутый текст
+        'strikethrough': f"~~{text}~~",  # Зачеркнутый текст
+    }
+    
+    return switcher.get(format_type, text)  # Возвращает текст без изменений, если формат не найден
