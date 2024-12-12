d9991b322e3face73bb741c4c238de1a3b88bcb5
pasha-danilevich <pasha-danilevich@outlook.com>
2024-12-12 23:54:18 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,16 +1,4 @@
 # src/database.py
 
 
-# Пример использования словаря для хранения данных (можно заменить на реальную БД)
-users = {}
-words = []
-
-def register_user(user_id, username):
-    users[user_id] = username
-
-def add_word(word):
-    words.append(word)
-
-
-
 
