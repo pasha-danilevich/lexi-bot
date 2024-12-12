0c4bb73b302107effb941ff4a45eaf5c152bc0e0
pasha-danilevich <pasha-danilevich@outlook.com>
2024-12-12 23:54:43 +0300

    Update login.py

--- src/handlers/login.py
+++ src/handlers/login.py
@@ -1,11 +1,11 @@
 # src/handlers/login.py
 
-from requests import status_codes
-from telebot import TeleBot
 import requests
+from telebot import TeleBot
 from telebot.types import Message
 from config import DOMAIN
 from handlers.welcome import get_buttons
+from user import User
 
 # Формируем URL для API
 API_URL = f'http://{DOMAIN}/api/jwt/create/'
@@ -14,18 +14,22 @@ def login_handler(bot: TeleBot, message: Message):
     bot.send_message(message.chat.id, "Введите ваш username или почту:")
     bot.register_next_step_handler(message, process_username, bot)
 
-def process_username(message, bot: TeleBot):
-    username = message.text.strip()
-    bot.send_message(message.chat.id, "Введите ваш пароль:")
-    bot.register_next_step_handler(message, process_password, username, bot)
+def process_username(message: Message, bot: TeleBot):
+    if message.text:
+        username = message.text.strip()
+        
+    user = User(email='', username=username, password='')  # Создаем объект User с введенным username
+    bot.send_message(message.chat.id, "Введите ваш пароль (убедитесь, что его никто не видит):")
+    bot.register_next_step_handler(message, process_password, user, bot)
 
-def process_password(message, username: str, bot: TeleBot):
-    password = message.text.strip()
+def process_password(message: Message, user: User, bot: TeleBot):
+    if message.text:
+        user.password = message.text.strip() 
     
     # Выполняем POST-запрос к API
     bot.send_message(message.chat.id, "Думаю...")
     
-    response = requests.post(API_URL, json={"username": username, "password": password})
+    response = requests.post(API_URL, json={"username": user.username, "password": user.password})
 
     if response.status_code == 200:
         tokens = response.json()
@@ -34,7 +38,7 @@ def process_password(message, username: str, bot: TeleBot):
 
         bot.send_message(message.chat.id, f"Вход выполнен успешно!\nAccess Token: {access_token[:5]}\nRefresh Token: {refresh_token[:5]}")
     elif response.status_code == 401:
-        text = "Ошибка при входе. Проверьте свои введенные данные данные."
+        text = "Ошибка при входе. Проверьте свои введенные данные."
         buttons = get_buttons()
         bot.send_message(message.chat.id, text, reply_markup=buttons)
     else:
