5bd8dc13eb7193f406b00f8fd8db6908527ea1f4
pasha-danilevich <pasha-danilevich@outlook.com>
2024-12-18 20:17:14 +0300

    my_profile view

--- src/handlers/my_profile.py
+++ src/handlers/my_profile.py
@@ -1,6 +1,8 @@
 from typing import Any
 
 import aiohttp
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.utils import markdown
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
@@ -23,7 +25,7 @@ async def view_token_handler(message: Message, state: FSMContext):
     access_token = data.get("access_token")
 
     if not access_token:
-        print("not access_token")
+        print("not access_token, get from db")
         from main import db
 
         user = await get_user(message=message, db=db)
@@ -40,7 +42,15 @@ async def view_token_handler(message: Message, state: FSMContext):
     if headers:
         home_data = await get_response_data(headers, home_url)
         training_data = await get_response_data(headers, training_url)
-        await message.answer(f"Ваш токен: {home_data}, {training_data}")
+        profile_info = await display_profile_info(home_data)
+        training_info = await display_training_info(training_data)
+
+        await message.answer(
+            text=profile_info, parse_mode=ParseMode.MARKDOWN_V2
+        )
+        await message.answer(
+            text=training_info, parse_mode=ParseMode.MARKDOWN_V2
+        )
     else:
         await message.answer("Токена нет.")
 
@@ -62,3 +72,40 @@ async def get_headers(access_token: str | Any | None) -> dict[str, str] | None:
         return headers
     else:
         return None
+
+
+async def display_profile_info(data) -> str:
+    # Пример данных
+    total_words = data["learning_words"]
+    added_books = data["upload_books"]
+    new_words_today = data["new_words_today"]
+
+    # Форматирование строки для вывода
+    profile_info = (
+        f"{markdown.bold('Ваш профиль:')} \n"
+        f"Всего слов: {markdown.bold(total_words)} \n"
+        f"Добавленных книг: {markdown.bold(added_books)} \n"
+        f"Новых слов за сегодня: {markdown.bold(new_words_today)} \n"
+    )
+    return profile_info
+
+
+async def display_training_info(data) -> str:
+
+    words_to_repeat = (
+        data["count_word_to_training_recognize"]
+        + data["count_word_to_training_reproduce"]
+    )
+    choice_test_words = data["count_word_to_training_recognize"]
+    reproduce_test_words = data["count_word_to_training_reproduce"]
+
+    # Форматирование строки для вывода
+    training_info = (
+        f"""Необходимо повторить {markdown.bold(words_to_repeat)} 
+        {markdown.bold('слов')} \n"""
+        f"Тест с выбором ответа: "
+        f"{markdown.bold(choice_test_words)}\n"
+        f"Тест на узнаваемость: "
+        f"{markdown.bold(reproduce_test_words)}\n"
+    )
+    return training_info
