9dc0ceed0e538044ece1de8f77f5c45eff2f31f7
pasha_danilevich <danilevitch.pasha@yandex.ru>
2025-05-17 23:02:40 +0300

    Поиск слова из all_word, если оно не найдено

--- bot/dialogs/add_word/interface.py
+++ bot/dialogs/add_word/interface.py
@@ -1,6 +1,12 @@
+from typing import TypedDict
+
 from bot.dialog_manager.manager import CustomDialogManagerImpl
 from bot.dialogs.add_word.dto import AddWordDTO
 from services.word.service import WordService
+
+
+class IStartData(TypedDict):
+    word: str
 
 
 class DialogManager(CustomDialogManagerImpl):
@@ -11,3 +17,8 @@ class DialogManager(CustomDialogManagerImpl):
     @property
     def dto(self) -> AddWordDTO:
         return super().dto
+
+    @property
+    def start_data(self) -> IStartData:
+        """Start data for current context."""
+        return self.current_context().start_data
